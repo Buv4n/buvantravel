20f093614e5212d5b5a687684b87ca7596b7d79d
Buv4n <82578960+Buv4n@users.noreply.github.com>
2026-03-27 20:09:58 +0530

    Delete backend directory

--- backend/src/routes/emailRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { sendContactEmail } from "../controllers/emailController.js";
-
-const router = express.Router();
-
-// POST /api/contact
-router.post("/", sendContactEmail);
-
-export default router;
